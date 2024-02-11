853a2e79051fdc8f29c99575327ec6d4c6e93afa
david gonzalez <97925031+david-gonzalez-coder@users.noreply.github.com>
2024-02-11 14:01:51 -0600

    title added to react library

--- src/pages/ReactLibrary.jsx
+++ src/pages/ReactLibrary.jsx
@@ -6,6 +6,8 @@ const Card = () => {
 export default function ReactLibrary() {
   return (
     <section>
+        <h1 className="text-center my-2 font-light text-3xl sm:text-4xl">React components</h1>
+        <p className="text-center">These are some components that I have made.</p>
         <div className='grid gap-4'>
             <div>
                 <UploadFilesArea />
